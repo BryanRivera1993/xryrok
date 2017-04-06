62fef95d0e595173dce727f5d6462a4f4badcd61
Bryan <bryanrivera@studentrnd.org>
2017-04-06 13:01:51 -0700

    Played around with NavBar

--- app/app.js
+++ app/app.js
@@ -8,14 +8,13 @@ import {
   Text,
   Navigator
 } from 'react-native';
-import { Screen, DropDownMenu, Divider, ScrollView, } from '@shoutem/ui/index';
-import { Stage } from '@shoutem/ui/examples/components/Stage';
+
 import { Nav } from './components/NavigationBar/Nav';
 
 export default class xryrok extends Component {
   render() {
     return (
-      <Nav />
+        <Nav /> 
     );
   }
 }
--- app/components/NavigationBar/Nav.js
+++ app/components/NavigationBar/Nav.js
@@ -2,14 +2,11 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  // View,
+  View,
+  Image,
   TouchableOpacity,
-  // Image,
-  // Text,
   ScrollView
 } from 'react-native';
-import { View } from '@shoutem/ui/components/View';
-import { Stage } from '@shoutem/ui/examples/components/Stage';
 import {
   NavigationBar,
   Heading,
@@ -18,26 +15,103 @@ import {
   Subtitle,
   Text,
   Caption,
-  Image,
   Button
-} from '@shoutem/ui/index'
+} from '@shoutem/ui/index';
+import { Hello } from './Hello';
+import { Hello2 } from './Hello2';
 
 
-export function Nav() {
+export class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentExample: Hello
+  }
+}
+
+  render(){
     return (
-        <View>
-          <Stage>
-              <NavigationBar
-                hasHistory
-                leftComponent={<Icon name="sidebar" />}
-                centerComponent={<Title>Xryrock Fuck YeaH</Title>}
-                rightComponent={(
-                  <Button styleName="clear">
-                    <Text>Report</Text>
-                  </Button>
-                )}
-              />
-          </Stage>
-        </View>
+            <View style={styles.container}>
+              <View style={styles.header}>
+                <Image
+                  source={{uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-3.png'}}
+                  style={{ width: 420, height: 70 }}
+                >
+                  <NavigationBar
+                    styleName="clear"
+                    hasHistory
+                    leftComponent={(
+                      <Button
+                      styleName="clear"
+                      onPress={this.onMenuPress.bind(this)}>
+                        <Icon name="sidebar" />
+                      </Button>
+                      )}
+                    centerComponent={<Heading>Xryrock</Heading>}
+                    rightComponent={(
+                      <Button styleName="clear">
+                        <Text>Report</Text>
+                      </Button>
+                    )}
+                  />
+                </Image>
+              </View>
+
+              <View style={styles.body}>
+                {this.renderContent()}
+              </View>
+            </View>
     );
 }
+
+renderContent() {
+    if (this.state.currentExample) {
+      const ExampleComponent = this.state.currentExample;
+      return <ExampleComponent />;
+    }
+
+    return (
+      <ScrollView style={styles.menuContainer}>
+        <TouchableOpacity onPress={this.onExamplePress.bind(this, Hello)}>
+          <Text style={styles.button2}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.onExamplePress.bind(this, Hello2)}>
+          <Text style={styles.button2}>Hello2</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    );
+}
+
+onExamplePress(currentExample) {
+    this.setState({currentExample});
+  }
+
+  onMenuPress() {
+    this.setState({currentExample: undefined});
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    backgroundColor: 'white',
+  },
+  header: {
+    height: 70,
+    flexDirection: 'row',
+    backgroundColor: '#5894f3',
+    alignItems: 'center',
+    zIndex: 1001
+  },
+  body: {
+      flex: 1,
+      zIndex: 1000
+  },
+  menuContainer: {
+    flex: 1,
+    paddingTop: 30,
+    paddingLeft: 40,
+    backgroundColor: 'white'
+  }
+});
